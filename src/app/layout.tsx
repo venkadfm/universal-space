0d6e08a93baea37a93c6e112a3fc22aeed370bf7
venkad <venkad@venkads-MacBook-Air.local>
2026-07-07 16:00:56 +0530

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -71,7 +72,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        {children}
+      </body>
+
+      <GoogleAnalytics gaId="G-44HMMJ18PY" />
     </html>
   );
 }
